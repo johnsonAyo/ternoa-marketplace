0e6e65244c1700a574efb49c6277fa24333548ce
ambiti0n <fabulousfresh010@gmail.com>
2022-06-08 14:00:55 +0100

    ternoa markrt place features

--- layouts/TopNavbarLayout.tsx
+++ layouts/TopNavbarLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 
-const TopNavbarLayout = ({ children }: { children: JSX.Element }) => {
+const TopNavbarLayout = ({ children }: any) => {
   return (
     <>
       <Header />
